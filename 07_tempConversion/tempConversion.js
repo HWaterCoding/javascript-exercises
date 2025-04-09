86b2985d7d3cbfc540e190d2daf1b09594e70b0a
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2025-04-09 12:42:42 -0400

    Complete exercise 7, all tests passed.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  temp = (fahrenheit - 32) * (5/9);
+  return Number(temp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  temp = celsius * 9 / 5 + 32;
+  return Number(temp.toFixed(1));
 };
 
 // Do not edit below this line
