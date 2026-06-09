10a1df09d619033459be8f821bfb9963555674bc
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-06-09 14:51:48 -0400

    Exercise 2 complete, all 9 tests pass.

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,22 @@
-const contains = function() {
-  
+const contains = function(object, searchValue) {
+    for (const key in object) {
+        const current = object[key];
+        if(
+            current === searchValue ||
+            Number.isNaN(current) && Number.isNaN(searchValue)
+        ){
+            return true;
+        }
+        if(
+            typeof current === "object" &&
+            current !== null
+        ){
+            if(contains(current, searchValue)){
+                return true;
+            }
+        }
+    }
+    return false;
 };
   
 // Do not edit below this line
