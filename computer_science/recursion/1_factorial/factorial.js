1695bfa48bc16b80817de436e5ddbc19e47210cf
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-06-09 14:17:37 -0400

    First exercise complete, all 11 tests pass

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,7 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if(n < 0 || n % 1 !==0 || typeof n !== "number") return undefined;
+    if(n <= 1) return 1;
+    return n * factorial(n - 1);
 };
 
 // Do not edit below this line
